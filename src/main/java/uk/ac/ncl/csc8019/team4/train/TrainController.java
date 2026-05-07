8fcd29b952fd628490dd13c773d42ab16e8025ba
chaowang55 <C5010508@newcastle.ac.uk>
2026-05-07 20:51:18 +0800

    Update TrainController.java

--- src/main/java/uk/ac/ncl/csc8019/team4/train/TrainController.java
+++ src/main/java/uk/ac/ncl/csc8019/team4/train/TrainController.java
@@ -19,6 +19,7 @@ import org.springframework.web.bind.annotation.RestController;
 @Validated
 @RestController
 @RequestMapping("/api/trains")
+    @CrossOrigin(origins = "*") 
 public class TrainController {
 
     private final TrainService trainService;
