9fffdc2262e0b78cab438bbebaaecfbe484ef888
chaowang55 <C5010508@newcastle.ac.uk>
2026-05-07 20:49:52 +0800

    Update MenuItemController.java

--- src/main/java/uk/ac/ncl/csc8019/team4/menu/MenuItemController.java
+++ src/main/java/uk/ac/ncl/csc8019/team4/menu/MenuItemController.java
@@ -12,6 +12,7 @@ import org.springframework.web.bind.annotation.*;
 import org.springframework.web.server.ResponseStatusException;
 
 @RestController
+    @CrossOrigin(origins = "*") 
 @RequestMapping("/api/menu")
 public class MenuItemController {
 
